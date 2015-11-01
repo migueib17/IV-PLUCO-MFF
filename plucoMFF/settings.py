5d3f5927e738f9cd5059d7149d846014506af6c5
migueib17 <miguel753951@gmail.com>
2015-11-01 08:55:41 +0100

    plucoMFF

---
+++ plucoMFF/settings.py
@@ -0,0 +1,12 @@
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+#Secret key
+SECRET_KEY = '\xb6\x98|\xba\xf3\x18\x9fk\xae_\xf7\xc6z\xff\x11\xad9\xf8\xe9xcH{e'
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
